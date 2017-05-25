164ee2a5d37f703b74161b48af7107193781d822
xianghao1986 <492598219@qq.com>
2017-05-26 00:14:09 +0800

    集成jpa

--- src/main/java/com/wtsd/TestController.java
+++ src/main/java/com/wtsd/TestController.java
@@ -2,6 +2,8 @@ package com.wtsd;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -12,8 +14,18 @@ public class TestController {
 
     private @Autowired PersonConfig config;
 
+    private @Autowired UserRepository userRepository;
     @RequestMapping(value = "/test")
     public String test() {
         return config.getName() + ":"+config.getAge();
+    }
+
+    @RequestMapping(value = "user/add", method = RequestMethod.GET)
+    public String add(@RequestParam(value = "name" ,defaultValue = "") String name, @RequestParam String email){
+        User user = new User();
+        user.setEmail(email);
+        user.setName(name);
+        userRepository.save(user);
+        return "保存成功";
     }
 }
